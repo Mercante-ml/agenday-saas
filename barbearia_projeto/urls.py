67d5bf3fc3fa1fc53533329e0d7e3619e7853f51
gustavo <g.mercante@hotmail.com>
2025-11-23 22:04:18 -0300

    Versão Inicial - Landing Page

--- barbearia_projeto/urls.py
+++ barbearia_projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Essa é a linha mágica:
+    # Quando acessar a raiz (''), mostre o template 'index.html'
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
